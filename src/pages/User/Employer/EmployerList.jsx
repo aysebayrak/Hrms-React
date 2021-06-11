cbbb2673426e75953b8a6d1612ac4a6e3b52509d
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-06-11 22:50:08 +0300

    .

--- src/pages/User/Employer/EmployerList.jsx
+++ src/pages/User/Employer/EmployerList.jsx
@@ -1,6 +1,6 @@
 import React ,{useState , useEffect} from 'react'
 import EmployerService from '../../../services/employerService';
-import { Table ,Header,  Icon  , Button } from 'semantic-ui-react'
+import { Table ,Header,  Icon  } from 'semantic-ui-react'
 
 export default function EmployerList() {
 
